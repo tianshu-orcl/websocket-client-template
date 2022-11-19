31e631ddf0f9673bfc043509d36012c803b75f1e
tianshu-orcl <tianshu.li@oracle.com>
2022-11-18 22:07:33 -0800

    add jest test case for generating client js/py code with the template

--- test/template/templateGenerationResult.test.js
+++ test/template/templateGenerationResult.test.js
@@ -5,20 +5,22 @@
 const { readFile } = require('fs').promises;
 const path = require('path');
 const Generator = require('@asyncapi/generator');
-const dummySpecUrl = 'https://rawcdn.githack.com/asyncapi/generator/v1.0.1/test/docs/dummy.yml';
-//you always want to generate to new directory to make sure test runs in clear environment
 const outputDir = path.resolve('test/temp/templateGenerationResult', Math.random().toString(36).substring(7));
 
 describe('templateGenerationResult()', () => {
-  jest.setTimeout(100000);
+  jest.setTimeout(1000000);
+
+  const params = {
+    server: 'localhost'
+  };
 
   beforeAll(async() => {
-    const generator = new Generator('./', outputDir, { forceWrite: true });
-/*    await generator.generateFromURL(dummySpecUrl); */
+    const generator = new Generator('./', outputDir, { forceWrite: true, templateParams: params });
+    await generator.generateFromFile(path.resolve('test','streaming.yaml')); 
   });
 
-  it('generated correct index.html with diagram source', async () => {
-/*     const index = await readFile(path.join(outputDir, 'index.html'), 'utf8'); */
-    /* expect(index).toMatchSnapshot(); */
+  it('generated correct streaming client code', async () => {
+    const clientJSCode = await readFile(path.join(outputDir, 'client.js'), 'utf8');
+    const clientPYCode = await readFile(path.join(outputDir, 'client.py'), 'utf8');
   });
 });
